e985929a62700d1d0c093e14a5bffe0784c56a68
Bricx Carasco <bricxraincarasco21@gmail.com>
2021-06-25 11:43:06 +0800

    create bootstrap html templates for site pages

--- profiler/settings.py
+++ profiler/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'profiler.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'views')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
