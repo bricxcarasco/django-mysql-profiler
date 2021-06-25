2ce13a0bb4ce72a007634a0b2d10d29c56225d41
Bricx Carasco <bricxraincarasco21@gmail.com>
2021-06-25 12:12:49 +0800

    Populate users record list in user html

--- users/views.py
+++ users/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+from .models import User
 
 # Create your views here.
 def index(request):
-    return render(request, 'users/index.html')
+    users = User.objects.order_by('-created_at')
+    context = {'users': users}
+    return render(request, 'users/index.html', context=context)
